cb0399eea40d3bdfcad30bb3215c7cdc24338023
imVic <victor.duran14@inacapmail.cl>
2025-11-22 16:08:14 -0300

    lo que hicimos en la biblioteca

--- registrar_departamento.php
+++ registrar_departamento.php
@@ -6,28 +6,19 @@ if (!$cont) {
     echo json_encode(['status' => 'error', 'message' => 'Error de conexión a la base de datos.']);
     exit();
 }
-
-$id_departamento = mysqli_real_escape_string($cont, $_GET['id_departamento']);
 $numero = mysqli_real_escape_string($cont, $_GET['numero']);
 $torre = mysqli_real_escape_string($cont, $_GET['torre']);
 $piso = mysqli_real_escape_string($cont, $_GET['piso']);
-$condominio = mysqli_real_escape_string($cont, $_GET['condominio']);
-
-$sql_check = "SELECT id_departamento FROM departamentos WHERE id_departamento = '$id_departamento'";
-$result_check = mysqli_query($cont, $sql_check);
-
-if (mysqli_num_rows($result_check) > 0) {
-    echo json_encode(['status' => 'error', 'message' => 'Este departamento ya se encuentra registrado.']);
-} else {
-    $sql_insert = "INSERT INTO departamentos (id_departamento, numero, torre, piso, condominio) 
-                   VALUES ('$id_departamento', '$numero', '$torre', '$piso', '$condominio')";
+$condominio = mysqli_real_escape_string($cont, $_GET['condominio']);    
+    $sql_insert = "INSERT INTO departamentos (numero, torre, piso, condominio) 
+                   VALUES ('$numero', '$torre', '$piso', '$condominio')";
 
     if (mysqli_query($cont, $sql_insert)) {
         echo json_encode(['status' => 'success', 'message' => 'Departamento registrado correctamente.']);
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Error al registrar el departamento: ' . mysqli_error($cont)]);
     }
-}
+
 
 mysqli_close($cont);
 ?>
--- sensor.php
+++ sensor.php
@@ -1,15 +1,21 @@
 <?php
+$cont = mysqli_connect('localhost', 'chris', 'Admin123', 'AppMovil');
 $inputJSON = file_get_contents('php://input');
 
 $input = json_decode($inputJSON, true);
 
 if (isset($input['uid'])) {
     $uid = $input['uid'];
-
+    $sql = "select * from sensores where mac_uid = '$uid' and activo = 1";
     file_put_contents('uids.txt', $uid . PHP_EOL, FILE_APPEND);
-    
-    echo json_encode(['status' => 'ok', 'uid' => $uid]);
-
+    $result = mysqli_query($cont, $sql);
+    $datos = mysqli_fetch_assoc($result);
+    if ($datos != null){
+        echo json_encode(['status' => 'ok', 'uid' => $uid]);
+    }
+    else {
+        echo json_encode(['status' => 'error', 'message' => 'El sensor no existe, o está bloqueado']);
+    }
 } else {
     echo json_encode(['status' => 'error', 'message' => 'UID no recibido']);
 }
